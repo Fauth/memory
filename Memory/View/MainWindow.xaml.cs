58a55e506c40da2cd9adcb4f1d118f9382bff5b5
PBiret <32267317+PBiret@users.noreply.github.com>
2017-11-02 21:08:11 +0100

    Created ItemsControl

--- Memory/View/MainWindow.xaml.cs
+++ Memory/View/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,31 @@ namespace Memory.View
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
+            var Cards = new ObservableCollection<Button>();
+
+            //Button C1 = new Button();   
+  
+            //C1.Margin = new Thickness((double)100, (double)10, (double)0, (double)0);
             InitializeComponent();
+
         }
+
+
+        private void C1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked Card 1", "J'ai perdu");
+
+        }
+
+        
+      
     }
 }
