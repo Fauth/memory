9b29df561c3f284dac057c16b5027cf644f73598
PBiret <32267317+PBiret@users.noreply.github.com>
2017-11-03 01:03:29 +0100

    Buttons are clickable and textured

--- Memory/View/MainWindow.xaml.cs
+++ Memory/View/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,25 +25,44 @@ namespace Memory.View
 
     public partial class MainWindow : Window
     {
+        int N = 10;
         public MainWindow()
         {
-            var Cards = new ObservableCollection<Button>();
+
+            
+
+
 
             //Button C1 = new Button();   
-  
+
             //C1.Margin = new Thickness((double)100, (double)10, (double)0, (double)0);
+
+            ObservableCollection<Button> Buttons = new ObservableCollection<Button>();
+            for (int k = 0; k<N; k++ )
+            { 
+             Buttons.Add(item: new Button() { Margin = new Thickness(10), Width = 127, Height = 200, Name= $"Card{k.ToString()}" });
+             Buttons[k].Click += Button_Click;
+             Buttons[k].Background = new ImageBrush(new BitmapImage(new Uri("pack://siteoforigin:,,,/Resources/ICO ("+(k+1).ToString()+").png")));
+            }
+
+
+            DataContext = Buttons;
             InitializeComponent();
-
         }
 
 
-        private void C1_Click(object sender, RoutedEventArgs e)
+        //private void C1_Click(object sender, RoutedEventArgs e)
+        //{
+        //    MessageBox.Show("You clicked Card 1", "Info");
+
+        //}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("You clicked Card 1", "J'ai perdu");
+            Button button_clicked = (Button)sender;
+            string buttonId = button_clicked.Name as string;
+            MessageBox.Show("You clicked " + buttonId, "Info");
 
         }
-
-        
-      
     }
 }
